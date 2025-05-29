7bed79a7c181f1612fd9c3886fea1f597e5da971
markbakosss <markbakosss@gmail.com>
2025-05-29 22:16:52 +0200

    added routes and error handler to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import {errorHandler} from "./middleware/errorHandler";
+
+import productRoutes from "./routes/productRoutes";
 
 const app = express();
 
@@ -7,5 +10,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Server is alive");
 })
+
+// Routes
+app.use('/api/products', productRoutes);
+
+// error handler
+app.use(errorHandler);
+
 
 export default app;
