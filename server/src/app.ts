a753058ddc0658fc80a1c46c2fc5fc6711fda60a
markbakosss <markbakosss@gmail.com>
2025-05-29 22:50:00 +0200

    added mongoose for mongodb connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import {errorHandler} from "./middleware/errorHandler";
 
 import productRoutes from "./routes/productRoutes";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("Server is alive");
 })
+
+// connect to database
+
+const mongoUri = process.env.MONGO_URI;
+if (!mongoUri) {
+    throw new Error("MONGO_URI is not defined in .env");
+}
+
+mongoose.connect(mongoUri)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err: unknown)=> console.error("MongoDB connection error:", err));
 
 // Routes
 app.use('/api/products', productRoutes);
