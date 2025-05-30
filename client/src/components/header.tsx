af5dd609dc73412a6ce78bf4eeb45f63334e3004
markbakosss <markbakosss@gmail.com>
2025-05-30 18:27:39 +0200

    added button component from shadcn and a better responsive design for header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
 import {Search} from "lucide-react";
 import Link from "next/link"
 import axios from "axios";
@@ -15,19 +16,22 @@ export default function Header() {
     }
 
     return (
-        <div className="flex w-screen h-24 items-center justify-center bg-white border">
+        <div className="flex w-screen h-24 items-center justify-center bg-white border space-x-4 lg:space-x-6 shadow-md">
             <div className="w-[30vw] lg:w-[16vw]">
                 <Link href="/">
-                    <h1 className="text-3xl font-bold text-blue-600 text-center pr-8 lg:p-0">
+                    <h1 className="text-3xl font-bold text-blue-600 text-center lg:p-0">
                         The Market
                     </h1>
                 </Link>
             </div>
             <div className="w-[50vw] flex items-center">
-                <Input onChange={(e) => {setSearchQuery(e)}} className="w-full h-12 pl-12" />
-                <div onClick={handleSearch} className="absolute m-1 w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center cursor-pointer">
-                    <Search className="w-5 h-5 text-white relative" />
+                <Input onChange={(e) => {setSearchQuery(e)}} className="w-full h-12 pl-12 lg:pl-3" />
+                <div onClick={handleSearch} className="lg:hidden absolute m-1 w-8 h-8 border border-blue-600 rounded-full flex items-center justify-center cursor-pointer">
+                    <Search className="w-5 h-5 text-blue-600 relative" />
                 </div>
+            </div>
+            <div className="hidden lg:flex">
+                <Button variant="default" className="bg-white border w-32 text-blue-600 h-12 hover:bg-blue-600 hover:text-white cursor-pointer"> Search </Button>
             </div>
         </div>
     )
