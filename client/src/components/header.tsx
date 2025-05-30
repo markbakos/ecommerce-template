7c66e8c3b5f420a1bcda676bcb762886eb05e8ab
markbakosss <markbakosss@gmail.com>
2025-05-30 18:12:11 +0200

    added searching to header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,9 +1,18 @@
+'use client';
+
 import {Input} from "@/components/ui/input";
 import {Search} from "lucide-react";
 import Link from "next/link"
+import axios from "axios";
+import {useState} from "react";
 
 export default function Header() {
+    const [searchQuery, setSearchQuery] = useState('');
 
+    async function handleSearch() {
+        const response = await axios.get(`http://localhost:3002/api/products/search?search=${searchQuery}`)
+        console.log(response)
+    }
 
     return (
         <div className="flex w-screen h-24 items-center justify-center bg-white border">
@@ -15,8 +24,8 @@ export default function Header() {
                 </Link>
             </div>
             <div className="w-[50vw] flex items-center">
-                <Input className="w-full h-12 pl-12" />
-                <div className="absolute m-1 w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center cursor-pointer">
+                <Input onChange={(e) => {setSearchQuery(e)}} className="w-full h-12 pl-12" />
+                <div onClick={handleSearch} className="absolute m-1 w-8 h-8 bg-blue-600 rounded-full flex items-center justify-center cursor-pointer">
                     <Search className="w-5 h-5 text-white relative" />
                 </div>
             </div>
