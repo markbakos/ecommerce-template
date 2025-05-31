fc5cb3b29af489f3cd7caedcc5208a9a2f0af760
markbakosss <markbakosss@gmail.com>
2025-05-31 14:19:39 +0200

    added ref to input to get text from input for pushing to url

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -4,16 +4,20 @@ import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
 import {Search} from "lucide-react";
 import Link from "next/link"
-import {useState} from "react";
+import {useRef} from "react";
 import {useRouter} from "next/navigation";
 
 export default function Header() {
-    const [searchQuery, setSearchQuery] = useState('');
+    const searchQuery = useRef(null);
 
     const router = useRouter();
 
     function handleSearch() {
-        router.push(`/search/${searchQuery}`);
+        if (!searchQuery.current || !searchQuery.current.value.trim()) {
+            return;
+        }
+
+        router.push(`/search/${searchQuery.current.value.trim()}`);
     }
 
     return (
@@ -26,13 +30,13 @@ export default function Header() {
                 </Link>
             </div>
             <div className="w-[50vw] flex items-center">
-                <Input onChange={(e) => {setSearchQuery(e)}} className="w-full h-12 pl-12 lg:pl-3" />
+                <Input ref={searchQuery} className="w-full h-12 pl-12 lg:pl-3" />
                 <div onClick={handleSearch} className="lg:hidden absolute m-1 w-8 h-8 border border-blue-600 rounded-full flex items-center justify-center cursor-pointer">
                     <Search className="w-5 h-5 text-blue-600 relative" />
                 </div>
             </div>
             <div className="hidden lg:flex">
-                <Button variant="default" className="bg-white border w-32 text-blue-600 h-12 hover:bg-blue-600 hover:text-white cursor-pointer"> Search </Button>
+                <Button onClick={handleSearch} variant="default" className="bg-white border w-32 text-blue-600 h-12 hover:bg-blue-600 hover:text-white cursor-pointer"> Search </Button>
             </div>
         </div>
     )
