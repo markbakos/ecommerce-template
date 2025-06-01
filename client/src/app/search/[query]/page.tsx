efe533863243cf7daa24acba6ec7d61fa39342dc
markbakosss <markbakosss@gmail.com>
2025-06-01 20:32:01 +0200

    Added fallback return while loading

--- client/src/app/search/[query]/page.tsx
+++ client/src/app/search/[query]/page.tsx
@@ -9,11 +9,13 @@ export default function SearchPage() {
     const params = useParams()
     const query = params.query as string;
 
+    const [loading, setLoading] = useState(false);
     const [results, setResults] = useState<Product[] | null>(null);
 
     useEffect(() => {
         async function fetchSearchResults() {
             try {
+                setLoading(true);
                 const response = await axios.get(`http://localhost:3002/api/products/search?search=${query}`);
                 if (response.status === 200) {
                     setResults(response.data)
@@ -26,23 +28,35 @@ export default function SearchPage() {
             catch (error) {
                 console.error("Error fetching search results:", error);
             }
-
-            console.log(results);
+            finally {
+                setLoading(false);
+            }
         }
 
         fetchSearchResults()
     }, [query]);
 
+    if (loading) {
+        return (
+            <div>
+                <Header />
+                <div className="w-screen flex justify-center items-center h-[80vh]">
+                    <h1 className="text-4xl">Loading...</h1>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <Header />
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center gap-4 pt-6">
                 { results && results.length > 0 ? (
                     results.map((product) => (
                         <SearchResultCard key={product._id} name={product.name} description={product.description} price={product.price} />
                     ))
                     ) : (
-                    <div>
+                    <div className="w-screen">
                         <h1>No results</h1>
                     </div>
                     )}
